c8bf252045ddeff788e2255362984b9fe857aed7
CandeZ <57143392+CandeZ@users.noreply.github.com>
2019-11-11 00:20:52 -0300

    Revert "Revert "changes""
    This reverts commit b51fa9c24d44a7631696051e95301cefe7264548.

--- Collision.cpp
+++ Collision.cpp
@@ -6,7 +6,7 @@ namespace Sonar{
 	}
 	bool Collision::CheckSpriteCollision( sf::Sprite sprite1 ){
 		sf::Rect<float> rect1 = sprite1.getGlobalBounds( );
-		sf::Rect<float> rect2(0,0,600,780);
+		sf::Rect<float> rect2(0,780,600,780);
 		
 		if( rect1.intersects ( rect2 ) ){
 			return true;
